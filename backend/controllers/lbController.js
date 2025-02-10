88639821df69834c509d15178177c70bdaf2ee4a
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-02-10 16:14:35 +0530

    removed files

--- backend/controllers/lbController.js
+++
@@ -1,6 +0,0 @@
-module.exports.getLeaderboard = async (req, res) => {
-  try {
-  } catch (error) {
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-};
--- backend/routes/leaderboard.js
+++
@@ -1,7 +0,0 @@
-const { getLeaderboard } = require("../controllers/lbController");
-
-const router = require("express").Router();
-
-router.get("/", getLeaderboard);
-
-module.exports = router;
